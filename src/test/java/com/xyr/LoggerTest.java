749fb7baa7280daaf59641cf9892cbbea9ac9be7
xyr <736845367@qq.com>
2017-09-19 17:42:28 +0800

    项目搭建

---
+++ src/test/java/com/xyr/LoggerTest.java
@@ -0,0 +1,31 @@
+package com.xyr;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by xyr on 2017/9/19.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+    //private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void test1() {
+        String name = "xyr";
+        String password = "123456";
+
+        log.debug("name:{},password:{}debug..........", name, password);
+        log.info("name:{},password:{}info................", name, password);
+        log.error("name:{},password:{}error............", name, password);
+    }
+
+}
